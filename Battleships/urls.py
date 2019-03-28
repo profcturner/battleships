e68ebb5b328ec94c1c7d1ce0117f5db83a9fb33c
Colin Turner <c.turner@ulster.ac.uk>
2019-03-28 13:59:44 +0000

    Our test server isn't running Django 2.x so we need to go back to older url syntax

--- Battleships/urls.py
+++ Battleships/urls.py
@@ -14,15 +14,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path
+# Commenting these for now since Feynman isn't (yet) on Django 2.x
+#from django.urls import path, re_path
+
+from django.conf.urls import include, url
 
 from Battleships.battleships import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/1.0/games/index', views.api_games_index),
-    path('api/1.0/players/index', views.api_players_index),
-    path('api/1.0/players/register/<name>', views.api_players_register),
-    path('api/1.0/players/delete/<name>', views.api_players_delete),
+    
+    url(r'^admin/', admin.site.urls),
+    url(r'^api/1.0/games/index', views.api_games_index),
+    url(r'^api/1.0/games/index', views.api_games_index),
+    url(r'^api/1.0/players/index', views.api_players_index),
+    url(r'^api/1.0/players/register/(P?<name>[A-Za-z_]+)', views.api_players_register),
+    url(r'^api/1.0/players/delete/(P?<name>[A-Za-z_]+)', views.api_players_delete),
 
 ]
