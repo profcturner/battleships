90b974c1b226a64a923f71c10db6d53590e700e1
Colin Turner <c.turner@ulster.ac.uk>
2019-04-01 18:58:17 +0100

    A number of tests for strike logic

--- Battleships/battleships/tests.py
+++ Battleships/battleships/tests.py
@@ -1,10 +1,88 @@
 from django.test import TestCase
 
+from .models import Action
 from .models import Game
+from .models import Location
 from .models import Player
 from .models import Ship
 
+class GameStrikeTestCase(TestCase):
+    """Test the mechanisms for ship strikes"""
+    def setUp(self):
+
+        # Create a game
+        game = Game.objects.create(name="test_game")
+
+        # Create and add players
+        p1 = Player.objects.create(name="player1")
+        p2 = Player.objects.create(name="player2")
+        p3 = Player.objects.create(name="player3")
+
+        game.players.add(p1)
+        game.players.add(p2)
+        game.players.add(p3)
+
+        # Create a single ship
+        game._create_ship_check('horizontal', p1, (3, 3), 3, name="Enterprise")
+
+
+    def test_count_locations(self):
+        # Test that the ship locations were added, there should be 3
+        game = Game.objects.get(name="test_game")
+        locations = Location.objects.all().filter(game=game)
+        ship = Ship.objects.get(name="Enterprise")
+
+        # The game should only have three locations stored so far
+        self.assertEqual(3, Location.objects.all().filter(game=game).count())
+
+        # Make sure the locations belong to the ship
+        self.assertEqual(3, ship.locations.all().count())
+
+
+    def test_check_for_hit_failure(self):
+        # Test that the check_if_hit model logic
+        game = Game.objects.get(name="test_game")
+
+        ship = game.check_for_hit((1,1))
+        self.assertIsNone(ship)
+
+
+    def test_check_for_hit_success(self):
+        # Test that the check_if_hit model logic
+        game = Game.objects.get(name="test_game")
+
+        ship = game.check_for_hit((3,3))
+        self.assertIsInstance(ship, Ship)
+
+
+    def test_strike_failure(self):
+        """Test a valid strike"""
+
+        game = Game.objects.get(name="test_game")
+        p1 = Player.objects.get(name="player1")
+
+        action = game.strike(p1, (1,1))
+        self.assertIsInstance(action, Action)
+        self.assertEqual(1, Ship.objects.all().filter(game=game).count())
+
+
+    def test_strike_success(self):
+        """Test a valid strike"""
+
+        game = Game.objects.get(name="test_game")
+        p1 = Player.objects.get(name="player1")
+
+        action = game.strike(p1, (3,3))
+        # We should have got a valid action
+        self.assertIsInstance(action, Action)
+        # There should be no more ships now!
+        self.assertEqual(0, Ship.objects.all().filter(game=game).count())
+
+
+
+
 class ShipCreationTestCase(TestCase):
+    """Test the mechanisms for ship creation"""
     def setUp(self):
 
         Game.objects.create(name="test_game")
