cd194c90a10221d87c98ce40c362c41173a54c25
Professor Colin Turner <c.turner@ulster.ac.uk>
2023-10-20 23:51:12 +0100

    Replacing removed url by re_path as required for Django 4.x

--- Battleships/urls.py
+++ Battleships/urls.py
@@ -15,30 +15,30 @@ Including another URLconf
 """
 from django.contrib import admin
 # Commenting these for now since Feynman isn't (yet) on Django 2.x
-#from django.urls import path, re_path
+from django.urls import path, re_path
 
-from django.conf.urls import include, url
+from django.conf.urls import include
 
 from server import views
 
 urlpatterns = [
 
-    url(r'^admin/', admin.site.urls),
-    url(r'^$', views.index),
-    url(r'^view_game/(?P<game_name>\w+)/$', views.view_game, name="view_game"),
-    url(r'^view_game/(?P<game_name>\w+)/(?P<player_id>\w+)/$', views.view_game),
-    url(r'^view_game/(?P<game_name>\w+)/(?P<player_name>\w+)/(?P<secret>\w+)/$', views.view_game),
-    url(r'^api/1.0/players/index/$', views.api_players_index),
-    url(r'^api/1.0/players/register/(?P<player_name>\w+)/$', views.api_players_register),
-    url(r'^api/1.0/players/delete/(?P<player_name>\w+)/(?P<secret>\w+)/$', views.api_players_delete),
-    url(r'^api/1.0/games/index/$', views.api_games_index),
-    url(r'^api/1.0/games/register/(?P<game_name>\w+)/$', views.api_games_register),
-    url(r'^api/1.0/games/delete/(?P<game_name>\w+)/(?P<secret>\w+)/$', views.api_games_delete),
-    url(r'^api/1.0/games/addplayer/(?P<game_name>\w+)/(?P<player_name>\w+)/$', views.api_games_add_player),
-    url(r'^api/1.0/games/start/(?P<game_name>\w+)/$', views.api_games_start_game),
-    url(r'^api/1.0/games/history/(?P<game_name>\w+)/$', views.api_games_history),
-    url(r'^api/1.0/games/getships/(?P<game_name>\w+)/(?P<player_name>\w+)/(?P<secret>\w+)/$', views.api_games_getships),
-    url(r'^api/1.0/games/getwinner/(?P<game_name>\w+)/$', views.api_games_getwinner),
-    url(r'^api/1.0/strike/(?P<game_name>\w+)/(?P<player_name>\w+)/\((?P<x>[0-9]+),(?P<y>[0-9]+)\)/(?P<secret>\w+)/$', views.api_strike),
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'^$', views.index),
+    re_path(r'^view_game/(?P<game_name>\w+)/$', views.view_game, name="view_game"),
+    re_path(r'^view_game/(?P<game_name>\w+)/(?P<player_id>\w+)/$', views.view_game),
+    re_path(r'^view_game/(?P<game_name>\w+)/(?P<player_name>\w+)/(?P<secret>\w+)/$', views.view_game),
+    re_path(r'^api/1.0/players/index/$', views.api_players_index),
+    re_path(r'^api/1.0/players/register/(?P<player_name>\w+)/$', views.api_players_register),
+    re_path(r'^api/1.0/players/delete/(?P<player_name>\w+)/(?P<secret>\w+)/$', views.api_players_delete),
+    re_path(r'^api/1.0/games/index/$', views.api_games_index),
+    re_path(r'^api/1.0/games/register/(?P<game_name>\w+)/$', views.api_games_register),
+    re_path(r'^api/1.0/games/delete/(?P<game_name>\w+)/(?P<secret>\w+)/$', views.api_games_delete),
+    re_path(r'^api/1.0/games/addplayer/(?P<game_name>\w+)/(?P<player_name>\w+)/$', views.api_games_add_player),
+    re_path(r'^api/1.0/games/start/(?P<game_name>\w+)/$', views.api_games_start_game),
+    re_path(r'^api/1.0/games/history/(?P<game_name>\w+)/$', views.api_games_history),
+    re_path(r'^api/1.0/games/getships/(?P<game_name>\w+)/(?P<player_name>\w+)/(?P<secret>\w+)/$', views.api_games_getships),
+    re_path(r'^api/1.0/games/getwinner/(?P<game_name>\w+)/$', views.api_games_getwinner),
+    re_path(r'^api/1.0/strike/(?P<game_name>\w+)/(?P<player_name>\w+)/\((?P<x>[0-9]+),(?P<y>[0-9]+)\)/(?P<secret>\w+)/$', views.api_strike),
 
 ]
